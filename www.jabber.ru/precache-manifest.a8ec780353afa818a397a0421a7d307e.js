8b20636263726ccdb113e017fe0bb30a8f4e2a9c
Anatoly Sablin <sablintolya@gmail.com>
2019-05-22 00:09:41 +0300

    j.ru-ng site.

---
+++ www.jabber.ru/precache-manifest.a8ec780353afa818a397a0421a7d307e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9233ee15e505708ae65972fc36ad4bab",
+    "url": "www.jabber.ru/index.html"
+  },
+  {
+    "revision": "1ae6cb241e63ad4484db",
+    "url": "www.jabber.ru/static/css/main.844220d8.chunk.css"
+  },
+  {
+    "revision": "ee3df397dfacd2357987",
+    "url": "www.jabber.ru/static/js/2.925ca5fc.chunk.js"
+  },
+  {
+    "revision": "1ae6cb241e63ad4484db",
+    "url": "www.jabber.ru/static/js/main.151cc7d5.chunk.js"
+  },
+  {
+    "revision": "ea54d523cc50ecc59bbd",
+    "url": "www.jabber.ru/static/js/runtime~main.02dee7f8.js"
+  }
+]);
